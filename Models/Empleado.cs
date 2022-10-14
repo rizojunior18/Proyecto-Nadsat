fd5f9693beffcecceb903d523c72b634cc28de9f
rizoj <rizoj@LAPTOP-GBLUOSU7>
2022-10-14 15:29:04 -0600

    Creacion de las clases principales

---
+++ Models/Empleado.cs
@@ -0,0 +1,22 @@
+﻿namespace Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Cedula { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+
+
+        public Empleado()
+        {
+
+        }
+
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Producto
+    {
+        
+        public int IdProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public string Marca { get; set; }
+        public double precio { get; set; }
+        public int CantidadDisponible { get; set; }
+
+
+        public Producto()
+        {
+
+        }
+    }
+
+    
+}
